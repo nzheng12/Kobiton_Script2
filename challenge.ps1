7e723bacbbcebdc45d5a54bdbdf74fb604a5a4f5
nzheng12 <55733024+nzheng12@users.noreply.github.com>
2021-03-14 18:35:31 -0400

    Add files via upload

---
+++ challenge.ps1
@@ -0,0 +1,37 @@
+﻿$firstOccurance=""
+$latestOccurance =$null
+Clear-Content  .\output.txt
+Get-ChildItem -Path .\logs |
+ForEach-Object{
+$content = Get-content $_.FullName
+$out =""
+$count=0
+$currentDateTime = $null
+$content | ForEach-Object {
+   
+   if($_ -match '(?<LogDatetime>\d+-\d+-\d+ \d+:\d+:\d+.\d+)'){
+       $currentDateTime = $Matches.LogDateTime
+   }
+   if($_ -match "MobileLabs.DeviceConnect.Framework.IO.PortableNetwork.PortableNetworkException" ){
+       if($count -eq 0 -and $firstOccurance -eq ""){
+           $firstOccurance = $currentDateTime;
+       }
+       if(-Not [string]::IsNullOrEmpty($latestOccurance)){
+           $currentDate = Get-Date $currentDateTime
+           $lastOccuranceDate = Get-Date $latestOccurance
+           if($lastOccuranceDate -lt $currentDate){
+               $latestOccurance = $currentDateTime
+           }
+       }else{
+           $latestOccurance = $currentDateTime
+       }
+       $count++ 
+    }
+
+   }
+   
+   $out = Write-Output $_.Name  $count
+   $out | Out-File -Append .\output.txt
+}
+"First Occurance: " + $firstOccurance | Out-File -Append .\output.txt
+"Latest Occurance: " + $latestOccurance |  Out-File -Append .\output.txt
